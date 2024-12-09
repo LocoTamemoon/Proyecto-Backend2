ab668bb8cfffe9aec7696b7f0aa1e13087724289
LocoTamemoon <leopoldvermilion22@gmail.com>
2024-12-09 15:06:09 -0500

    Update TareaService.java

--- src/main/java/com/gestion/tasking/service/TareaService.java
+++ src/main/java/com/gestion/tasking/service/TareaService.java
@@ -35,4 +35,39 @@ public class TareaService {
     public List<Tarea> obtenerTareasPorProyecto(int idProyecto) {
         return tareaDAO.obtenerTareasPorProyecto(idProyecto);
     }
+
+
+
+
+    	public Tarea actualizarTarea(int idTarea, String nombre, String descripcion, Integer prioridad, Integer estado,
+			LocalDate fechaVencimiento) throws Exception {
+		
+        // Convertir LocalDate a String en el formato adecuado (yyyy-MM-dd)
+        String fechaFormateada = fechaVencimiento.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        
+        try {
+    		return tareaDAO.actualizarTarea(idTarea, nombre, descripcion, prioridad, estado, fechaFormateada);
+        } catch (Exception e) {
+            // Si ocurre un error, propagar la excepción con el mensaje adecuado
+            throw new RuntimeException(e.getMessage()); // Excepción con el mensaje conciso
+        }
+	}
+
+	public void eliminarTarea(int idTarea) throws Exception {
+		tareaDAO.eliminarTarea(idTarea);
+	}
+	
+	
+	public boolean existeTarea(int idTarea) throws Exception {
+	    return tareaDAO.existeTarea(idTarea);
+	}
+
+
+
+
+
+
+
+
+    
 }
